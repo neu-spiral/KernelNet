4897bac4a43acc97c7e2641a7d5dfcbc9c8fb092
chieh <juliusctw@gmail.com>
2018-10-06 12:54:26 -0400

    trying to gen data for 10 fold

--- src/helper/dataset_manipulate.py
+++ src/helper/dataset_manipulate.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+from path_tools import *
+import os
+
+def gen_10_fold_data(db):
+	#	Does the 10 fold path exist
+	ten_fold_exist = True
+	for i in range(0,10):
+		train_path = ('%s/10_fold/split_%d/train.csv'%(db['data_path'],i))
+		test_path = ('%s/10_fold/split_%d/test.csv'%(db['data_path'],i))
+		train_label_path = ('%s/10_fold/split_%d/train_label.csv'%(db['data_path'],i))
+		test_label_path = ('%s/10_fold/split_%d/test_label.csv'%(db['data_path'],i))
+
+		path_list = [train_path, train_label_path, test_path, test_label_path] 
+		if not path_list_exists(path_list): ten_fold_exist = False
+
+
+	if db['recompute_10_fold'] == False and ten_fold_exist == True: return
+
+
+	ensure_path_exists('%s/10_fold'%(db['data_path']))
+	for i in range(0,10):
+		split_folder = ('%s/10_fold/split_%d'%(db['data_path'],i))
+		ensure_path_exists(split_folder)
+
+
+	#	Split data into 10 and save them accordingly
+
--- tests/test_parent.py
+++ tests/test_parent.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
+import dataset_manipulate 
 from termcolor import colored
 import numpy as np
 
 
 class test_parent():
-	def __init__(self):
-		pass
+	def __init__(self,db):
+		self.db = db
+		db['data_path'] = './datasets/' + db['data_name'] + '/'
+		
 
 	def run(self):
-		print('ran')
+		dataset_manipulate.gen_10_fold_data(self.db)
 
 
 
