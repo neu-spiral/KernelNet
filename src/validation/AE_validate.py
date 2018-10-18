dd7e543d985cd2ebaf5958294f2f75028b9e5f09
chieh <juliusctw@gmail.com>
2018-10-17 20:37:20 -0400

    not important

--- src/validation/AE_validate.py
+++ src/validation/AE_validate.py
@@ -4,7 +4,71 @@ from dataset_manipulate import *
 from terminal_print import *
 from path_tools import *
 from classifier import *
+import time
 import pickle
+
+def save_results_to_text_file(db, result_path, fname, output_str):
+	if 'running_batch_mode' not in db: 
+		most_recent_result_path = result_path + fname
+		mr_mutex = result_path + fname + '.writing_mutex'
+
+		while os.path.exists(mr_mutex): time.sleep(20*np.random.rand())
+
+		create_file(mr_mutex)
+		fin = open(most_recent_result_path, 'w')
+		fin.write(output_str)
+		fin.close()
+		delete_file(mr_mutex)
+
+def save_result_to_history(db, result, result_path, fname, output_str):
+	file_path = result_path + fname + '.pk'
+	mutex = result_path + fname + '.writing'
+	tmp_writing = result_path + fname + '.' + str(int(10000000*np.random.rand()))
+
+
+	if path_list_exists([file_path]):
+		past_runs = pickle.load( open( file_path, "rb" ) )
+
+		past_runs['list_of_runs'].append(result)
+
+		if past_runs['best_train_nmi']['train_nmi'] < result['train_nmi']:
+			past_runs['best_train_nmi'] = result
+			save_results_to_text_file(db, result_path, 'best_train_nmi.txt' , output_str)
+
+		if past_runs['best_valid_nmi']['valid_nmi'] < result['valid_nmi']:
+			past_runs['best_valid_nmi'] = result
+			save_results_to_text_file(db, result_path, 'best_valid_nmi.txt' , output_str)
+
+		if past_runs['best_train_loss']['train_loss'] > result['train_loss']:
+			past_runs['best_train_loss'] = result
+			save_results_to_text_file(db, result_path, 'lowest_train_loss.txt' , output_str)
+
+		if past_runs['best_valid_loss']['valid_loss'] > result['valid_loss']:
+			past_runs['best_valid_loss'] = result
+			save_results_to_text_file(db, result_path, 'lowest_valid_loss.txt' , output_str)
+
+	else:
+		result['knet'] = db["knet"]
+
+		past_runs = {}
+		past_runs['list_of_runs'] = [result]
+		past_runs['best_train_nmi'] = result
+		past_runs['best_valid_nmi'] = result
+		past_runs['best_train_loss'] = result
+		past_runs['best_valid_loss'] = result
+
+		save_results_to_text_file(db, result_path, 'best_train_nmi.txt' , output_str)
+		save_results_to_text_file(db, result_path, 'best_valid_nmi.txt' , output_str)
+		save_results_to_text_file(db, result_path, 'lowest_train_loss.txt' , output_str)
+		save_results_to_text_file(db, result_path, 'lowest_valid_loss.txt' , output_str)
+
+	pickle.dump( past_runs, open(tmp_writing, "wb" ) )
+
+
+	while os.path.exists(mutex): time.sleep(20*np.random.rand())
+	create_file(mutex)
+	shutil.move(tmp_writing, file_path)
+	delete_file(mutex)
 
 def AE_validate(db):
 	#	get loss objective
