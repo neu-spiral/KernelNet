40115ef82cd768a356a717e25d758936f2bc38db
“chieh-neu” <juliusctw@gmail.com>
2018-10-19 10:49:43 -0400

    not important

--- results/read_history.py
+++ results/read_history.py
@@ -2,9 +2,13 @@
 
 import pickle
 import os
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt; plt.rcdefaults()
 
 
 project_name = 'wine'
+run_list = {}
 
 for i in range(20):
 	fold_id = str(i) + '_'
@@ -12,6 +16,7 @@ for i in range(20):
 
 	history = pickle.load(open('./' + project_name + '/' + fold_id + 'run_history.pk','rb'))
 	
+	run_list[i] = history['list_of_runs']
 	num_of_runs = len(history['list_of_runs'])
 	best_train_nmi = history['best_train_nmi']
 	best_valid_nmi = history['best_valid_nmi']
@@ -40,5 +45,28 @@ for i in range(20):
 	print('\t\tvalid nmi : %.3f'%best_valid_nmi['valid_nmi'])
 	print('\t\ttrain loss : %.3f'%best_valid_nmi['train_loss'])
 	print('\t\tvalid loss : %.3f'%best_valid_nmi['valid_loss'])
+
+
+X_valid = []
+Y_valid = []
+X_train = []
+Y_train = []
+
+for result in run_list[0]:
+	X_valid.append(result['valid_loss'])
+	Y_valid.append(result['valid_nmi'])
+	X_train.append(result['train_loss'])
+	Y_train.append(result['train_nmi'])
+
+
+plt.figure(figsize=(14,5))
+plt.subplot(121)
+plt.plot(X_train, Y_train, 'x')
+plt.title('Train loss vs NMI')
+plt.subplot(122)
+plt.plot(X_valid, Y_valid, 'x')
+plt.title('Validation loss vs NMI')
+
+plt.show()
 
 
