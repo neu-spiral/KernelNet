348c1ccf26309a39d66fb50744761bbbeb47ae56
“chieh-neu” <juliusctw@gmail.com>
2018-10-19 10:09:40 -0400

    not important

--- results/read_history.py
+++ results/read_history.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python
 
 import pickle
+import os
 
 
 project_name = 'wine'
-fold_id = '0_'
 
-history = pickle.load(open('./' + project_name + '/' + fold_id + 'run_history.pk','rb'))
+for i in range(20):
+	fold_id = str(i) + '_'
+	if not os.path.exists('./' + project_name + '/' + fold_id + 'run_history.pk'): continue
 
-num_of_runs = len(history['list_of_runs'])
-best_train_nmi = history['best_train_nmi']
-best_valid_nmi = history['best_valid_nmi']
-best_train_loss = history['best_train_loss']
-best_valid_loss = history['best_valid_loss']
-
-
-print('Num of runs : %d'%num_of_runs)
-print('Best nmi based on train loss')
-print('\tnmi : %.3f'%best_train_loss['train_nmi'])
-print('\tloss : %.3f'%best_train_loss['train_loss'])
-print('Best nmi based on valid loss')
-print('\tnmi : %.3f'%best_valid_loss['train_nmi'])
-print('\tloss : %.3f'%best_valid_loss['train_loss'])
-print('Best overall train nmi')
-print('\tnmi : %.3f'%best_train_nmi['train_nmi'])
-print('\tloss : %.3f'%best_train_nmi['train_loss'])
-print('Best overall valid nmi')
-print('\tnmi : %.3f'%best_valid_nmi['train_nmi'])
-print('\tloss : %.3f'%best_valid_nmi['train_loss'])
+	history = pickle.load(open('./' + project_name + '/' + fold_id + 'run_history.pk','rb'))
+	
+	num_of_runs = len(history['list_of_runs'])
+	best_train_nmi = history['best_train_nmi']
+	best_valid_nmi = history['best_valid_nmi']
+	best_train_loss = history['best_train_loss']
+	best_valid_loss = history['best_valid_loss']
+	
+	
+	print('\nRun ID : %s with Num of runs : %d'%(fold_id, num_of_runs))
+	print('\tBest nmi based on train loss')
+	print('\t\ttrain nmi : %.3f'%best_train_loss['train_nmi'])
+	print('\t\tvalid nmi : %.3f'%best_train_loss['valid_nmi'])
+	print('\t\ttrain loss : %.3f'%best_train_loss['train_loss'])
+	print('\t\tvalid loss : %.3f'%best_train_loss['valid_loss'])
+	print('\tBest nmi based on valid loss')
+	print('\t\ttrain nmi : %.3f'%best_valid_loss['train_nmi'])
+	print('\t\tvalid nmi : %.3f'%best_valid_loss['valid_nmi'])
+	print('\t\ttrain loss : %.3f'%best_valid_loss['train_loss'])
+	print('\t\tvalid loss : %.3f'%best_valid_loss['valid_loss'])
+	print('\tBest overall train nmi')
+	print('\t\ttrain nmi : %.3f'%best_train_nmi['train_nmi'])
+	print('\t\tvalid nmi : %.3f'%best_train_nmi['valid_nmi'])
+	print('\t\ttrain loss : %.3f'%best_train_nmi['train_loss'])
+	print('\t\tvalid loss : %.3f'%best_train_nmi['valid_loss'])
+	print('\tBest overall valid nmi')
+	print('\t\ttrain nmi : %.3f'%best_valid_nmi['train_nmi'])
+	print('\t\tvalid nmi : %.3f'%best_valid_nmi['valid_nmi'])
+	print('\t\ttrain loss : %.3f'%best_valid_nmi['train_loss'])
+	print('\t\tvalid loss : %.3f'%best_valid_nmi['valid_loss'])
 
 
