f59d6d3bbbc1af732b136336b12e315b30b920fb
“chieh-neu” <juliusctw@gmail.com>
2018-10-29 11:18:42 -0400

    this version optimizes U, but report U normalized for NMI

--- src/optimizer/opt_Kernel.py
+++ src/optimizer/opt_Kernel.py
@@ -13,17 +13,25 @@ class opt_K():
 		print('\t' + str(count) + ' : Computing K...')
 
 		db = self.db
-		U = ensure_matrix_is_numpy(db['U'])
-		if 'Ku' not in db: db['Ku'] = U.dot(U.T)
+		if 'Ku' not in db: db['Ku'] = db['U'].dot(db['U'].T)
 	
 		Y = center_matrix(db, db['Ku'])
 		if db['use_Degree_matrix']: Y = db['D_inv'].dot(Y).dot(db['D_inv'])	
 		np.fill_diagonal(Y, 0)
 		Y = numpy2Variable(Y, db['dataType'])
 
+		#print(db['train_data'].X[0:10,0:4])
+		#[db['x_hat'], db['ϕ_x']] = db['knet'](db['train_data'].X_Var)		
+		#print(db['ϕ_x'][0:10,0:4])
+
+
 		db['knet'].set_Y(Y)
-		[avgLoss, avgGrad, progression_slope] = basic_optimizer(db['knet'], db, loss_callback='compute_loss', data_loader_name='train_loader', epoc_loop=100)
+		[avgLoss, avgGrad, progression_slope] = basic_optimizer(db['knet'], db, loss_callback='compute_loss', data_loader_name='train_loader', epoc_loop=20)
 		[db['x_hat'], db['ϕ_x']] = db['knet'](db['train_data'].X_Var)		# <- update this to be used in opt_K
+
+		#print(db['ϕ_x'][0:10,0:4])
+		#import pdb; pdb.set_trace()
+		#print(db['train_data'].X[0:5,0:4])
 
 		if 'objective_tracker' in db:
 			if 'running_batch_mode' in db: return
@@ -31,12 +39,21 @@ class opt_K():
 			φ_x = ensure_matrix_is_numpy(db['ϕ_x'])
 			[DKxD, Dinv] = normalized_rbk_sklearn(φ_x, db['knet'].σ)
 			HDKxDH = center_matrix(db, DKxD)
+
 			current_hsic = -float(np.sum(HDKxDH*db['Ku']))
 			current_AE_loss = float(ensure_matrix_is_numpy(db['knet'].autoencoder_loss(db['train_data'].X_Var, None, None)))
 			current_loss = float(current_hsic + db['λ']*current_AE_loss)
 
 			db['objective_tracker'] = np.append(db['objective_tracker'], current_loss)
-			[allocation, train_nmi] = kmeans(db['num_of_clusters'], U, Y=db['train_data'].Y)
+
+
+
+			#[allocation, train_nmi] = kmeans(db['num_of_clusters'], db['U'], Y=db['train_data'].Y)
+			[allocation, train_nmi] = kmeans(db['num_of_clusters'], db['U_normalized'], Y=db['train_data'].Y)
+
+
+
+
 			print('\t\tCurrent obj loss : %.5f from %.5f +  (%.3f)(%.3f)[%.5f]'%(current_loss, current_hsic, db["λ_ratio"], db['λ_obj_ratio'], current_AE_loss))
 			print('\t\tTrain NMI after optimizing θ : %.3f'%(train_nmi))
 
@@ -52,14 +69,26 @@ class opt_U():
 		φ_x = ensure_matrix_is_numpy(db['ϕ_x'])
 		[DKxD, Dinv] = normalized_rbk_sklearn(φ_x, db['knet'].σ)
 		HDKxDH = center_matrix(db, DKxD)
-		[U, U_normalized] = L_to_U(db, HDKxDH)
+		
+	
 
-		db['U_prev'] = db['U']
-		if db['use_U_normalize']: db['U'] = U_normalized 	# <- update this to be used in opt_K
-		else: db['U'] = U
+		#print(db['U'][0:4,0:3])
+		#[U, U_normalized] = L_to_U(db, HDKxDH)
+		[db['U'], db['U_normalized']] = L_to_U(db, HDKxDH)
+
+
+
+		#db['U_prev'] = db['U']
+		#if db['use_U_normalize']: db['U'] = U_normalized 	# <- update this to be used in opt_K
+		#else: db['U'] = U
+
 		db['prev_Ku'] = db['Ku']
 		db['Ku'] = db['U'].dot(db['U'].T)
 	
+
+		#print(db['U'][0:4,0:3])
+
+
 		if 'objective_tracker' in db:
 			if 'running_batch_mode' in db: return
 
@@ -70,7 +99,7 @@ class opt_U():
 			db['objective_tracker'] = np.append(db['objective_tracker'], current_loss)
 
 			#[allocation, train_nmi] = kmeans(db['num_of_clusters'], U_normalized, Y=db['train_data'].Y)
-			[allocation, train_nmi] = kmeans(db['num_of_clusters'], db['U'] , Y=db['train_data'].Y)
+			[allocation, train_nmi] = kmeans(db['num_of_clusters'], db['U_normalized'] , Y=db['train_data'].Y)
 			print('\t\tCurrent obj loss : %.5f from %.5f +  (%.3f)(%.3f)[%.5f]'%(current_loss, current_hsic, db["λ_ratio"], db['λ_obj_ratio'], current_AE_loss))
 			print('\t\tTrain NMI after optimizing U : %.3f'%(train_nmi))
 
@@ -84,9 +113,9 @@ def exit_cond(db, count):
 	error_per_element = '%.2f' % error_per_element
 	db['converge_list'].append(error_per_element)
 
-	clear_previous_line()
+	#clear_previous_line()
 	print('\t\tBetween U, Kx error Per element : ' + str(db['converge_list']))
-	if float(error_per_element) <= 0.02:
+	if float(error_per_element) <= 0.01:
 		db['knet'].itr_til_converge = count
 		exit_count = 100
 	else:
--- src/validation/AE_validate.py
+++ src/validation/AE_validate.py
@@ -104,7 +104,7 @@ def AE_validate(db):
 		#[db['valid_loss'], db['valid_hsic'], db['valid_AE_loss'], φ_x] = db['knet'].get_current_state(db, db['valid_data'].X_Var)
 
 	#	get training nmi
-	current_label = kmeans(db['num_of_clusters'], db['U'])
+	current_label = kmeans(db['num_of_clusters'], db['U_normalized'])
 	db['train_nmi'] = normalized_mutual_info_score(current_label, db['train_data'].Y)
 
 	[x_hat, ϕ_x] = db['knet'](db['train_data'].X_Var)
@@ -161,7 +161,7 @@ def AE_validate(db):
 
 	output_str += '\tResults of Initial States '
 	packet_5 = {}
-	list_of_keys = ['init_spectral_nmi', 'init_AE+Kmeans_nmi', 'initial_loss', 'initial_hsic', 'initial_AE_loss']
+	list_of_keys = ['init_spectral_nmi', 'init_AE+Kmeans_nmi', 'init_AE+Spectral_nmi', 'initial_loss', 'initial_hsic', 'initial_AE_loss']
 	fill_dictionary(db, packet_5, list_of_keys)
 	output_str += '\n' + dictionary_to_str(packet_5)
 
